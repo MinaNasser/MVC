48892d2c40f2c74a929f4d61d7e67e5b5b425584
MinaAl5al <minanasser82018@gmail.com>
2025-03-30 23:34:15 +0200

    Attribute

---
+++ WebApp/Filtters/MyCustomAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Filtters
+{
+    public class MyCustomAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
